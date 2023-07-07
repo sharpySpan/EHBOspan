cf1c949146211752ef1e6e773e567ad07b510500
Span, Yoran - Student Rijn IJssel <00320374@rijnijssel.nl>
2023-07-07 11:09:31 +0200

    intitial

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/home')
+def home():
+    return render_template("home.html")
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+@app.route('/cursus')
+def cursus():
+    return render_template("cursus.html")
 
 if __name__ == '__main__':
     app.run()
