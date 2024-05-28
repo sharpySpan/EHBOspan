ea7ff1a01cb1da400519c0d230b4494bf9327aac
Span, Yoran - Student Rijn IJssel <00320374@rijnijssel.nl>
2024-05-28 10:47:46 +0200

    .gitignore

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pymysql.cursors
 app = Flask(__name__)
 app.secret_key = 'ietsiets'
 
-connection = pymysql.connect(host="localhost", user="root", passwd="", database="flipquiz")
+connection = pymysql.connect(host="localhost", user="root", passwd="", database="dnd")
 cursor = connection.cursor()
 
 
@@ -16,7 +16,7 @@ def login():
         email = request.form['email']
         password = request.form['password']
 
-        cursor.execute('SELECT * FROM users WHERE email = %s AND password = %s', (email, password))
+        cursor.execute('SELECT * FROM user WHERE username = %s AND password = %s', (email, password))
         data = cursor.fetchone()
         session['id'] = data[0]
 
@@ -25,26 +25,26 @@ def login():
 
         if data:
             session['loggedin'] = True
-            return redirect(url_for('quiz_Home'))
+            return flask.render_template('home.html')
     else:
-        return flask.render_template('home.html')
+        return flask.render_template('login.html')
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        name = request.form['name']
-        school_name = request.form['school_name']
-        subject = request.form['subject']
 
-        cursor.execute('INSERT INTO users (email, password, name, schoolname, subject ) values (%s, %s, %s, %s, %s)', (email, password, name, school_name, subject))
+        cursor.execute('INSERT INTO user (username, password ) values (%s, %s)', (email, password))
         connection.commit()
 
-        return flask.render_template('home.html')
+        return flask.render_template('login.html')
     else:
         return flask.render_template('register.html')
 
+@app.route('/home')
+def home():
+    return flask.render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
